a7b929546c4db202e958bcfcb4ca3fdd976be753
naw linlin min <nodmentor1ygn@gmail.com>
2020-10-09 19:46:51 +0630

    edit routes.rb

---
+++ config/application.rb
@@ -0,0 +1,23 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module InstagramClone
+  class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+    config.load_defaults 6.0
+    config.generators do |g|
+      # The description in these two lines creates a Settings that is not automatically generated.
+      g.assets false
+      g.helper false
+    end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  root to: 'users#new'
+      resources :sessions, only: [:new, :create, :destroy]
+      resources :users, only: [:new, :create, :show, :edit, :update]
+      resources :favorites, only: [:create, :destroy]
+      get '/users/:id/favorites', to: 'users#favorites'
+      resources :pictures do
+        collection do
+          post :confirm
+        end
+      end
+      mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  end
